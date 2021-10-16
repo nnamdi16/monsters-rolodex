81e3a4d8fee47456afb3bb7b522605a30e86cd82
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-10-16 11:59:02 +0100

    Created a card list component

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,9 @@
+import { Card } from '../card/card.component';
+import './card-list.style.css'
+export const CardList = (props) => {
+    return ( < div className="card-list">
+      {props.monsters.map(monster => (
+          <Card key = {monster.id} monsters={monster}/>
+      ))}
+        </div>)
+}
