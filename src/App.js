a5d7ad31cf6460a0110c9d79b5cbc014daefd880
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-10-16 12:49:22 +0100

    Added the serach component and created a method called the handleChange method and bounded the this context to the App Component by using  arrow function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
 // function App() {
 //   return (
@@ -30,9 +31,14 @@ class App extends Component {
       monsters: [],
       searchField: ''
     }
+
   }
   componentDidMount() {
     fetch(`https://jsonplaceholder.typicode.com/users`).then(response => response.json()).then(users => this.setState({monsters:users}))
+  }
+
+  handleChange = e => {
+    return this.setState({searchField: e.target.value});
   }
   render() {
     const {monsters, searchField} = this.state;
@@ -40,10 +46,10 @@ class App extends Component {
     )
       return (
     <div className="App">
-      <input type="search" 
-              placeholder="Search monsters"
-              onChange={e =>this.setState({searchField: e.target.value})
-              }/>
+      <SearchBox
+      placeholder="Search monsters"
+      handleChange={this.handleChange}
+      />
       <CardList monsters={filteredMonsters}>
      
       </CardList>
