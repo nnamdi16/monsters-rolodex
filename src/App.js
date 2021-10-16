2a0773ef4e1c78fbdf07a20956c267bd45f12a8b
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-10-16 12:17:40 +0100

    Implement search feature by filtering the the list of monsters based on the input values of the search field

--- src/App.js
+++ src/App.js
@@ -35,13 +35,16 @@ class App extends Component {
     fetch(`https://jsonplaceholder.typicode.com/users`).then(response => response.json()).then(users => this.setState({monsters:users}))
   }
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+    )
       return (
     <div className="App">
       <input type="search" 
               placeholder="Search monsters"
               onChange={e =>this.setState({searchField: e.target.value})
               }/>
-      <CardList monsters={this.state.monsters}>
+      <CardList monsters={filteredMonsters}>
      
       </CardList>
     </div>
